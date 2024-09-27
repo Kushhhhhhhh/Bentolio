4d1a8037c11d9ce2c41cc04d5dfc77cf4f1009d1
kush sharma <kush.73888@gmail.com>
2024-09-27 17:30:04 +0530

    Bentolio Completed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,101 +1,178 @@
+"use client";
+
 import Image from "next/image";
+import Link from "next/link";
+import { ArrowUpRight, Linkedin, Github, Mail } from 'lucide-react'
+import { useEffect, useRef } from 'react';
+import { motion, useAnimation } from 'framer-motion';
+import { gsap } from 'gsap';
+import { ScrollTrigger } from 'gsap/ScrollTrigger';
+
+gsap.registerPlugin(ScrollTrigger);
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="https://nextjs.org/icons/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
+  const mainImageRef = useRef(null);
+  const scrollRef = useRef(null);
+  const controls = useAnimation();
+
+  useEffect(() => {
+    gsap.fromTo(
+      mainImageRef.current,
+      { opacity: 0, scale: 0.8 },
+      { opacity: 1, scale: 1, duration: 1, ease: "power3.out" }
+    );
+
+    gsap.utils.toArray('.scroll-trigger').forEach((element) => {
+      if (element instanceof Element) {
+        gsap.fromTo(element, 
+          { y: 50, opacity: 0 },
+          {
+            y: 0,
+            opacity: 1,
+            duration: 1,
+            scrollTrigger: {
+              trigger: element,
+              start: "top bottom-=100",
+              end: "bottom top+=100",
+              toggleActions: "play none none reverse"
+            }
+          }
+        );
+      }
+    });
+
+    controls.start("visible");
+  }, [controls]);
+
+  const containerVariants = {
+    hidden: { opacity: 0 },
+    visible: {
+      opacity: 1,
+      transition: {
+        delayChildren: 1,
+        staggerChildren: 0.2
+      }
+    }
+  };
+
+  const itemVariants = {
+    hidden: { y: 20, opacity: 0 },
+    visible: {
+      y: 0,
+      opacity: 1,
+      transition: {
+        duration: 0.5,
+        ease: "easeOut"
+      }
+    }
+  };
+
+  return (
+    <motion.div
+      ref={scrollRef}
+      className="bg-black min-h-dvh p-3 md:p-8 text-gray-200"
+      initial="hidden"
+      animate={controls}
+      variants={containerVariants}
+    >
+      <motion.header variants={itemVariants} className="flex flex-col bg-[#D4C6B8] rounded-2xl p-2 sm:flex-row justify-between items-center mb-8">
+        <h1 className="text-xl font-bold font-serif italic text-[#4A3728] ml-4 mb-4 sm:mb-0">
+          Kush Sharma
+        </h1>
+        <nav className="space-x-4 mr-4">
+          <Link href="https://kushsharma.vercel.app/#projects" className="text-sm text-[#6B4D30] hover:text-[#4A3728]">
+            PROJECTS
+          </Link>
+          <Link href="https://kushsharma.vercel.app/#about" className="text-sm text-[#6B4D30] hover:text-[#4A3728]">
+            ABOUT
+          </Link>
+          <Link href="https://kushsharma.vercel.app/#contact" className="text-sm text-[#6B4D30] hover:text-[#4A3728]">
+            CONTACT
+          </Link>
+        </nav>
+      </motion.header>
+
+      <div className="grid grid-cols-12 gap-4">
+        <motion.div variants={itemVariants} className="bg-[#D4C6B8] p-6 rounded-md col-span-12 md:col-span-6 lg:col-span-4 flex flex-col justify-evenly scroll-trigger">
+          <motion.div variants={itemVariants} className="flex justify-end items-center">
+            <Image 
+            src="/coffee.png"
+            alt="coffee"
+            width={100}
+            height={100}
+            className="object-cover hover:scale-110 transition-all duration-300 ease-in-out mb-4 mr-4"
+            priority
             />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+          </motion.div>
+          <h2 className="text-2xl md:text-3xl font-bold mb-2 text-[#4A3728]">
+            <span className="font-mono italic underline">Next.js </span>
+            <span className="font-bold">Full Stack Wizard</span> <span className="font-thin">Creating Seamless, </span>
+            <span className="font-bold"> High-Impact</span> <span className="font-thin"> Digital Experiences</span>
+            <span className="font-bold font-mono"> That Inspire</span>
+          </h2>
+        </motion.div>
+
+        <div ref={mainImageRef} className="bg-[#565449] rounded-md flex items-end justify-center main_image col-span-12 md:col-span-5 lg:col-span-4">
+          <Image
+            src="/bentolio-avatar.png"
+            alt="Kush Sharma"
+            width={240}
+            height={240}
+            className="max-w-[240px] object-cover pt-12 hover:scale-95 transition-all duration-300 ease-in-out"
+            priority
+          />
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+
+        <motion.div variants={itemVariants} className="bg-[#D4C6B8] p-4 rounded-md flex flex-col justify-evenly scroll-trigger col-span-12 md:col-span-7 lg:col-span-4">
+          <div className="flex justify-between items-center mb-4">
+            <h3 className="text-2xl font-semibold text-[#4A3728]">Work</h3>
+            <ArrowUpRight className="text-[#6B4D30] cursor-pointer" size={16} />
+          </div>
+          <div className="flex flex-col items-center">
+            <Image
+              src="/work.png"
+              alt="Vision Forge Project"
+              width={400}
+              height={250}
+              className="rounded-lg mb-4 object-cover hover:scale-105 transition-all duration-300 ease-in-out"
+              priority
+            />
+            <div className="flex flex-col items-start p-2 px-4">
+            <h3 className="text-2xl font-semibold text-[#4A3728] mb-2 text-left">Vision Forge</h3>
+            <p className="text-sm font-thin text-left text-black">
+            VisionForge is a dynamic project, allows users to generate stunning images for free using prompts, with unlimited generations available.
+            </p>
+            </div>
+          </div>
+        </motion.div>
+
+        <motion.div variants={itemVariants} className="bg-[#D4C6B8] p-6 rounded-md scroll-trigger col-span-12 md:col-span-8 lg:col-span-6">
+          <p className="text-lg font-thin font-mono mb-4 text-center text-black">
+          A dedicated and driven developer with a strong passion for creating meaningful digital experiences. Known for a problem-solving mindset, attention to detail, and a genuine curiosity to learn and grow. Always ready to tackle challenges with persistence and a commitment to delivering quality results.
+          </p>
+        </motion.div>
+
+        <motion.div variants={itemVariants} className="bg-[#565449] p-6 rounded-md scroll-trigger col-span-12 md:col-span-4 lg:col-span-3 flex flex-col justify-between">
+          <div className="flex justify-between items-center">
+            <h3 className="text-lg font-thin mb-2 text-[#ffddc3]">Have some questions?</h3>
+            <ArrowUpRight className="mb-2 text-[#D4C6B8] cursor-pointer" size={20} />
+          </div>
+          <h3 className="text-4xl md:text-5xl mt-4 font-mono font-semibold text-[#ffecdd]">Contact me</h3>
+        </motion.div>
+
+        <motion.div variants={itemVariants} className="bg-[#D4C6B8] p-4 rounded-md flex justify-evenly items-center scroll-trigger col-span-12 md:col-span-6 lg:col-span-3">
+          {[
+            { href: 'https://www.linkedin.com/in/kushsharma738', Icon: Linkedin },
+            { href: 'mailto:fullstack.kush@gmail.com', Icon: Mail },
+            { href: 'https://github.com/Kushhhhhhhh', Icon: Github }
+          ].map(({ href, Icon }) => (
+            <Link key={href} href={href} className="text-[#6B4D30] hover:text-[#4A3728]">
+              <Icon size={30} />
+            </Link>
+          ))}
+        </motion.div>
+      </div>
+    </motion.div>
   );
 }
